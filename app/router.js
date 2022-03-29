54199199b0e555bdaec4a0f0dccdb3bce7463c0f
Huansong Zeng <huansongzeng@Huansongs-MBP.modem>
2022-03-30 00:05:27 +1100

    [function] get weather api

--- app/router.js
+++ app/router.js
@@ -1,8 +1,28 @@
 const { Router } = require('express');
 const router = Router();
+const axios = require('axios')
 
 router.get('/', async (req,res) => {
-    res.send('Hello World!');
+    const response = await axios.get('http://www.bom.gov.au/fwo/IDN60801/IDN60801.95765.json');
+    const { status, data } = response;
+    
+    if (status === 200) {
+        let weatherData = data.observations.data
+        let returnData = weatherData
+                            .filter(record => record.apparent_t > 20)
+                            .sort((a,b) => a.apparent_t - b.apparent_t)
+                            .map(record => { 
+                                return {
+                                    name: record.name,
+                                    apparent_t: record.apparent_t,
+                                    lat: record.lat,
+                                    long: record.lon,
+                                }
+                            })
+        res.json(returnData)
+    } else {
+        res.status(503).json({"error": "Error Connecting to BOM."})
+    }
 })
 
 module.exports = router
