3f109bc762f1efeac95e19b67dbe2b49fd2fa3ee
Huansong Zeng <huansongzeng@Huansongs-MBP.modem>
2022-03-31 09:45:11 +1100

    update test and readme

--- app/__tests__/routes.t.js
+++ app/__tests__/routes.t.js
@@ -8,11 +8,11 @@ app.use('/', router)
 describe('route test', function(){
     const targetAttr = ["name", "apparent_t", "lat", "long"]
 
-    test('response to/', async () => {
+    it('response to/', async () => {
         const res = await request(app).get('/');
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
-        expect(res.statusCode).toBe(200);
-        expect(res.body.length).toEqual(164);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.length > 0).toEqual(true);
         expect(Object.keys(res.body[0]).length).toEqual(4)
         for (let attr of targetAttr){
             expect(attr in res.body[0]).toBe(true)
