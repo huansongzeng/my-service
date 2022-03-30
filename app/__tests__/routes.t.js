4706c74b77b3ffb03abdefabe6c34dc4ef8dfb2f
Huansong Zeng <huansongzeng@Huansongs-MBP.modem>
2022-03-30 20:50:48 +1100

    [function] unit test && change if to switch

---
+++ app/__tests__/routes.t.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const express = require('express');
+const router = require('../router');
+
+const app = new express();
+app.use('/', router)
+
+describe('route test', function(){
+    const targetAttr = ["name", "apparent_t", "lat", "long"]
+    test('response to/', async () => {
+        const res = await request(app).get('/');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.length).toEqual(164);
+        expect(Object.keys(res.body[0]).length).toEqual(4)
+        for (let attr of targetAttr){
+            expect(attr in res.body[0]).toBe(true)
+        }
+    })
+})
--- app/router.js
+++ app/router.js
@@ -5,23 +5,24 @@ const axios = require('axios')
 router.get('/', async (req,res) => {
     const response = await axios.get('http://www.bom.gov.au/fwo/IDN60801/IDN60801.95765.json');
     const { status, data } = response;
-    
-    if (status === 200) {
-        let weatherData = data.observations.data
-        let returnData = weatherData
-                            .filter(record => record.apparent_t > 20)
-                            .sort((a,b) => a.apparent_t - b.apparent_t)
-                            .map(record => { 
-                                return {
-                                    name: record.name,
-                                    apparent_t: record.apparent_t,
-                                    lat: record.lat,
-                                    long: record.lon,
-                                }
-                            })
-        res.json(returnData)
-    } else {
-        res.status(503).json({"error": "Error Connecting to BOM."})
+    switch(status) {
+        case 200:
+            let weatherData = data.observations.data
+            let returnData = weatherData
+                                .filter(record => record.apparent_t > 20)
+                                .sort((a,b) => a.apparent_t - b.apparent_t)
+                                .map(record => { 
+                                    return {
+                                        name: record.name,
+                                        apparent_t: record.apparent_t,
+                                        lat: record.lat,
+                                        long: record.lon,
+                                    }
+                                })
+            res.json(returnData)
+            break
+        default:
+            res.status(503).json({"error": "Error Connecting to BOM."})
     }
 })
 
